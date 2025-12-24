6922ea7866427b1c0ef9c916162eb0add82a1c51
Alex Alan Nunes <alexalannunes@gmail.com>
2025-12-24 14:41:04 -0300

    update ExploreProductsButton layout and enhance product description for Mini Compressor

--- components/products/explore-products-button.tsx
+++ components/products/explore-products-button.tsx
@@ -25,7 +25,7 @@ export function ExploreProductsButton() {
             }
           }}
         >
-          Explorar o Catálogo <ArrowLeft className="ml-2 h-5 w-5" />
+          <ArrowLeft className="ml-2 h-5 w-5" /> Explorar o Catálogo
         </Button>
       </Link>
     </div>
